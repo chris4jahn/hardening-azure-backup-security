314c59892bdd5540daba0ca131bf989760b0c427
chris4jahn <christian.forjahn@outlook.com>
2025-11-25 05:47:23 +0100

    Remove Azure Backup Vault module and related files, transitioning to Azure Recovery Services Vault module with enhanced security features and updated documentation.

--- CLI/commands.sh
+++ CLI/commands.sh
@@ -1,57 +1,54 @@
-az account set --subscription 33d063dc-2392-4d6f-ba58-3128b84aa2fc 
+### Demo Part 1: Create and List Recovery Services Vaults
+# create a resource group named rg-bofh-backup
+az group create --name rg-bofh-backup --location swedencentral 
 
-az backup vault create --resource-group bofh-backupvault01 --name bofhbackup-vault01 --location swedencentral --subscription sub-mvp-cf-2025 --output table
-
-Copilot: 
-I want to create another recovery service vault named bofh-rs-vault111125 in the same resource group bofh-backupvault01. Generate the Azure CLI command for that
-az backup vault create --resource-group bofh-backupvault01 --name bofh-rs-vault111125 --location swedencentral --subscription sub-mvp-cf-2025 --output table
-
-az backup vault list --output table
+# Create a Recovery Services vault
+az backup vault create --resource-group rg-bofh-backup --name bofh-rsvault03 --location swedencentral
 
 Copilot: 
-I want to delete bofh-rs-vault111125. Generate an Azure CLI command for that
+I want to delete bofh-rsvault03 in RG rg-bofh-backup. Generate an Azure CLI command for that
 CLI
-az backup vault delete --name bofh-rs-vault111125 --resource-group bofh-backupvault01 --yes 
-
-az backup vault delete --name bofhbackup-vault01 --resource-group bofh-backupvault01 --yes 
-
-Copilot:
-whats the cli command to get my container
-CLI:
-az backup container list --resource-group bofh-backupvault01 --vault-name bofhbackup-vault01 --backup-management-type AzureIaaSVM
-
-Copilot:
-How do I recover a deleted Recovery Services vault?
+az backup vault delete --name bofh-rsvault03 --resource-group rg-bofh-backup --yes 
 
 
+
+### Demo Part 2: Delete Recovery Services Vault with Protected Items using Azure PowerShell
 Copilot: 
-I want to delete bofhbackup-vault01. There are protected items in the recovery service vault. How can I delete the vault using Azure PowerShell? I want to fill in all names using get commands or foreach loops. Fill in SubscriptionId and the conatiner name for each protected item. Add BackupManagementType and Workloadtype AzureVM for the items. I want to copy and paste it directly to the Azure cloud shell
+I want to delete bofh-rsvault02. There are protected items in the recovery service vault. How can I delete the vault using Azure PowerShell? I want to fill in all names using get commands or foreach loops. Fill in SubscriptionId and the conatiner name for each protected item. Add BackupManagementType and Workloadtype AzureVM for the items. I want to copy and paste it directly to the Azure cloud shell
 PowerShell:
 
                         # Delete all backup items in Recovery Services Vault
                         Write-Host "Switching to subscription $subscriptionId..." -ForegroundColor Cyan
-                        Set-AzContext -SubscriptionId 33d063dc-2392-4d6f-ba58-3128b84aa2fc
+                        Set-AzContext -SubscriptionId <your-subscription-id>
+                
                         Write-Host "Deleting all backup items in vault $vaultId..." -ForegroundColor Yellow
-                        $backupItemsVM = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureVM -WorkloadType AzureVM -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
-                        $backupItemsSQL = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType MSSQL -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
-                        $backupItemsAFS = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureStorage -WorkloadType AzureFiles -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
-                        $backupItemsSAP = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType SAPHanaDatabase -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
+                        $backupItemsVM = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureVM -WorkloadType AzureVM -VaultId /subscriptions/<your-subscription-id>
+                /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
+                        $backupItemsSQL = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType MSSQL -VaultId /subscriptions/<your-subscription-id>
+                /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
+                        $backupItemsAFS = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureStorage -WorkloadType AzureFiles -VaultId /subscriptions/<your-subscription-id>
+                /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
+                        $backupItemsSAP = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType SAPHanaDatabase -VaultId /subscriptions/<your-subscription-id>
+                /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01
                         foreach ($item in $backupItemsVM) {
                                 Write-Host "Removing VM backup item: $($item.Name)" -ForegroundColor DarkYellow
-                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
+                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/<your-subscription-id>
+                        /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
                         }
                         foreach ($item in $backupItemsSQL) {
                                 Write-Host "Removing SQL backup item: $($item.Name)" -ForegroundColor DarkYellow
-                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
+                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/<your-subscription-id>
+                        /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
                         }
                         foreach ($item in $backupItemsSAP) {
                                 Write-Host "Removing SAP HANA backup item: $($item.Name)" -ForegroundColor DarkYellow
-                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
+                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/<your-subscription-id>
+                        /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
                         }
                         foreach ($item in $backupItemsAFS) {
                                 Write-Host "Removing Azure Files backup item: $($item.Name)" -ForegroundColor DarkYellow
-                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/33d063dc-2392-4d6f-ba58-3128b84aa2fc/resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
+                                Disable-AzRecoveryServicesBackupProtection -Item $item -VaultId /subscriptions/<your-subscription-id>
+                        /resourceGroups/bofh-backupvault01/providers/Microsoft.RecoveryServices/vaults/bofhbackup-vault01 -RemoveRecoveryPoints -Force
                         }
                         Write-Host "All backup items deleted." -ForegroundColor Green
                 
-Set-AzContext -Subscription 33d063dc-2392-4d6f-ba58-3128b84aa2fc 
